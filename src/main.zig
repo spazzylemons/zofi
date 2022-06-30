368b2b79c8eb2604469076a82ce148e2e891ee7b
spazzylemons <25674682+spazzylemons@users.noreply.github.com>
2022-06-30 14:53:15 -0400

    add search bar to filter executable list

--- src/main.zig
+++ src/main.zig
@@ -61,63 +61,85 @@ fn searchPath() !ExeMap {
     return result;
 }
 
-fn extractLabel(row: ?*g.c.GtkListBoxRow) [*:0]const u8 {
-    const children = g.c.gtk_container_get_children(g.cast(g.c.GtkContainer, row, g.c.gtk_container_get_type()));
-    const label = g.cast(g.c.GtkLabel, children.*.data, g.c.gtk_label_get_type());
-    return g.c.gtk_label_get_text(label);
-}
+const Client = struct {
+    exes: []const []const u8,
+    entry_list: *g.c.GtkWidget = undefined,
+    entry: *g.c.GtkEntry = undefined,
 
-fn sortRows(row1: ?*g.c.GtkListBoxRow, row2: ?*g.c.GtkListBoxRow, user_data: ?*anyopaque) callconv(.C) g.c.gint {
-    // NOTE: this does not handle identical strings as they should not be encountered
-    _ = user_data;
-    var p = extractLabel(row1);
-    var q = extractLabel(row2);
-    while (true) {
-        const x = @as(g.c.gint, p[0]);
-        const y = @as(g.c.gint, q[0]);
-        if (x != y) {
-            return x - y;
+    fn rebuildList(self: *Client) void {
+        const input = std.mem.span(g.c.gtk_entry_get_text(self.entry));
+
+        const list_box_widget = g.c.gtk_list_box_new();
+        const list_box = g.cast(g.c.GtkListBox, list_box_widget, g.c.gtk_list_box_get_type());
+        const list_box_container = g.cast(g.c.GtkContainer, list_box, g.c.gtk_container_get_type());
+
+        var buf: [std.os.PATH_MAX + 1]u8 = undefined;
+
+        for (self.exes) |exe| {
+            if (std.mem.indexOf(u8, exe, input) == null) continue;
+            std.mem.copy(u8, &buf, exe);
+            buf[exe.len] = 0;
+            const label = g.c.gtk_label_new(&buf);
+            g.c.gtk_container_add(list_box_container, label);
         }
-        p += 1;
-        q += 1;
+
+        const scrolled_window_widget = g.c.gtk_scrolled_window_new(null, null);
+        const scrolled_window = g.cast(g.c.GtkScrolledWindow, scrolled_window_widget, g.c.gtk_scrolled_window_get_type());
+
+        g.c.gtk_container_add(g.cast(g.c.GtkContainer, scrolled_window, g.c.gtk_container_get_type()), list_box_widget);
+        g.c.gtk_scrolled_window_set_min_content_height(scrolled_window, 320);
+        g.c.gtk_scrolled_window_set_min_content_width(scrolled_window, 640);
+
+        g.c.gtk_list_box_set_selection_mode(list_box, g.c.GTK_SELECTION_BROWSE);
+
+        if (@ptrCast(?*g.c.GList, g.c.gtk_container_get_children(list_box_container))) |child_node| {
+            g.c.gtk_list_box_select_row(list_box, g.cast(g.c.GtkListBoxRow, child_node.data, g.c.gtk_list_box_row_get_type()));
+        }
+
+        const box_container = g.cast(g.c.GtkContainer, g.c.gtk_widget_get_parent(self.entry_list), g.c.gtk_container_get_type());
+        g.c.gtk_container_remove(box_container, self.entry_list);
+        g.c.gtk_container_add(box_container, scrolled_window_widget);
+        self.entry_list = scrolled_window_widget;
+        g.c.gtk_widget_show_all(scrolled_window_widget);
     }
+};
+
+fn onChanged(editable: ?*g.c.GtkEditable, self: *Client) callconv(.C) void {
+    _ = editable;
+    self.rebuildList();
 }
 
-fn onActivate(app: *g.c.GtkApplication, exe_map: *ExeMap) callconv(.C) void {
+fn onActivate(app: *g.c.GtkApplication, self: *Client) callconv(.C) void {
     const window_widget = g.c.gtk_application_window_new(app);
     const window = g.cast(g.c.GtkWindow, window_widget, g.c.gtk_window_get_type());
     g.c.gtk_layer_init_for_window(window);
     g.c.gtk_layer_set_layer(window, g.c.GTK_LAYER_SHELL_LAYER_TOP);
     g.c.gtk_layer_set_keyboard_mode(window, g.c.GTK_LAYER_SHELL_KEYBOARD_MODE_ON_DEMAND);
 
-    const scrolled_window_widget = g.c.gtk_scrolled_window_new(null, null);
-    const scrolled_window = g.cast(g.c.GtkScrolledWindow, scrolled_window_widget, g.c.gtk_scrolled_window_get_type());
+    const box_widget = g.c.gtk_box_new(g.c.GTK_ORIENTATION_VERTICAL, 0);
+    const box_container = g.cast(g.c.GtkContainer, box_widget, g.c.gtk_container_get_type());
 
-    const list_box_widget = g.c.gtk_list_box_new();
-    const list_box = g.cast(g.c.GtkListBox, list_box_widget, g.c.gtk_list_box_get_type());
-    const list_box_container = g.cast(g.c.GtkContainer, list_box, g.c.gtk_container_get_type());
-    g.c.gtk_list_box_set_sort_func(list_box, sortRows, null, null);
+    const command_entry_widget = g.c.gtk_entry_new();
+    const command_entry = g.cast(g.c.GtkEntry, command_entry_widget, g.c.gtk_entry_get_type());
+    g.c.gtk_entry_set_icon_from_icon_name(command_entry, g.c.GTK_ENTRY_ICON_PRIMARY, "edit-find");
+    g.c.gtk_container_add(box_container, command_entry_widget);
+    g.signalConnect(command_entry, "changed", onChanged, self);
 
-    var it = exe_map.data.keyIterator();
-    while (it.next()) |key| {
-        const string = allocator.dupeZ(u8, key.*) catch {
-            std.log.warn("failed to allocate entry {s}", .{key.*});
-            continue;
-        };
-        defer allocator.free(string);
-        const label = g.c.gtk_label_new(string.ptr);
-        g.c.gtk_container_add(list_box_container, label);
-    }
-
-    g.c.gtk_container_add(g.cast(g.c.GtkContainer, scrolled_window, g.c.gtk_container_get_type()), list_box_widget);
-    g.c.gtk_scrolled_window_set_min_content_height(scrolled_window, 320);
-    g.c.gtk_scrolled_window_set_min_content_width(scrolled_window, 640);
+    self.entry = command_entry;
+    self.entry_list = g.c.gtk_label_new("");
+    g.c.gtk_container_add(box_container, self.entry_list);
+    self.rebuildList();
 
     const window_container = g.cast(g.c.GtkContainer, window, g.c.gtk_container_get_type());
-    g.c.gtk_container_add(window_container, scrolled_window_widget);
+    g.c.gtk_container_add(window_container, box_widget);
     g.c.gtk_container_set_border_width(window_container, 12);
     // TODO - large ListBox is slow, consider loading elements on-demand somehow
     g.c.gtk_widget_show_all(window_widget);
+}
+
+fn stringLessThan(context: void, p: []const u8, q: []const u8) bool {
+    _ = context;
+    return std.mem.order(u8, p, q) == .lt;
 }
 
 pub fn main() u8 {
@@ -129,9 +151,22 @@ pub fn main() u8 {
     };
     defer exe_map.deinit();
 
+    var sorted = std.ArrayListUnmanaged([]const u8){};
+    defer sorted.deinit(allocator);
+
+    var it = exe_map.data.keyIterator();
+    while (it.next()) |key| {
+        sorted.append(allocator, key.*) catch {
+            std.log.err("out of memory", .{});
+            return 1;
+        };
+    }
+    std.sort.sort([]const u8, sorted.items, {}, stringLessThan);
+    var client = Client{ .exes = sorted.items };
+
     const app = g.c.gtk_application_new("spazzylemons.zofi", g.c.G_APPLICATION_FLAGS_NONE);
     defer g.c.g_object_unref(app);
-    g.signalConnect(app, "activate", onActivate, &exe_map);
+    g.signalConnect(app, "activate", onActivate, &client);
     const g_app = g.cast(g.c.GApplication, app, g.c.g_application_get_type());
     return @truncate(u8, @bitCast(u32, g.c.g_application_run(g_app, 0, null)));
 }
